c5db7c257a7e09e847d66ed483a6642a12ed102f
Anderson Silva <andssilva.23@gmail.com>
2022-02-24 00:56:34 -0400

    fix(req 4): :rocket: conserta erros no requisito

--- src/insights.py
+++ src/insights.py
@@ -66,22 +66,15 @@ def get_max_salary(path):
     """
     Passos a se seguir:
     1 - Buscar os dados que foram encontrados na def jobs.read
-    2 - Criar um array que vai receber os valores de máximos salários
-    3 - Percorrer o array de dados do def jobs.read inserindo apenas
-        os salários no novo array
-    4 - Verificar se o salário não é vazio ou inválido
-    5 - Percorrer o novo array verificando qual é o maior salário
+    2 - Verificar se o salário não é vazio ou inválido
+    3 - Percorrer o novo array verificando qual é o maior salário
     """
     list_all_jobs = read(path)
-    salary_list = []
     max_salary = 0
     for salary in list_all_jobs:
-        if salary["max_salary"] != "":
-            if salary["max_salary"] != "invalid":
-                salary_list.append(salary["max_salary"])
-    for max_sal in salary_list:
-        if max_salary < int(max_sal):
-            max_salary = int(max_sal)
+        if salary["max_salary"] != "" and salary["max_salary"] != "invalid":
+            if max_salary < int(salary["max_salary"]):
+                max_salary = int(salary["max_salary"])
     return max_salary
 
 
